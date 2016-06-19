16a8341582110fd052cc3ac2c392c527e14b4a6f
KorayiDC <korayece2@live.de>
2016-06-19 18:05:57 +0200

    Erster commit

---
+++ Berlin/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Berlin
+//
+//  Created by Koray Ece on 08.06.16.
+//  Copyright © 2016 Koray Ece. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    var webView: WKWebView!
+    var detailItem: [String: String]!
+    
+    @IBAction func dismiss(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true, completion: {});
+        
+    }
+    override func loadView() {
+        webView = WKWebView()
+        view = webView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "<body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
+        
+    }
+}
---
+++ Berlin/MasterViewController.swift
@@ -0,0 +1,144 @@
+//
+//  MasterViewController.swift
+//  Berlin
+//
+//  Created by Koray Ece on 08.06.16.
+//  Copyright © 2016 Koray Ece. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    @IBAction func aboutPressed(sender: AnyObject) {
+        
+        
+        let alert = UIAlertController(title: "Version 1.2", message: "Mit dieser App, sehen sie alle aktuellen Veranstaltungen in Berlin und Brandenburg.", preferredStyle: UIAlertControllerStyle.Alert)
+        
+        alert.addAction(UIAlertAction(title: "Made by Koray Ece", style: UIAlertActionStyle.Default, handler: nil))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+        
+        
+    }
+  
+    
+    @IBAction func safariPressed(sender: AnyObject) {
+        
+       UIApplication.sharedApplication().openURL(NSURL(string: "http://daten.berlin.de")!)
+    }
+    
+    
+    
+    var detailViewController: DetailViewController? = nil
+    var objects = [[String: String]]()
+    var url:NSURL? = NSURL(string: "daten.berlin.de")
+  
+    
+
+    
+    
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+
+    
+        let urlString = "http://www.berlin.de/sen/wirtschaft/service/maerkte-feste/strassen-volksfeste/index.php/index/all.json?q="
+    
+        if let url = NSURL(string: urlString) {
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                let json = JSON(data: data)
+                
+                if json["messages"] != nil {
+                    // jetzt gehts los
+                    parseJson(json)
+                    print(json)
+                    
+                
+                } else {
+                    print("Fehler. Eventuell keine Internetverbindung?")
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    func parseJson(json: JSON) {
+        
+        for index in json["index"].arrayValue {
+            
+            let title = index["bezeichnung"].stringValue
+            let body = index["strasse"].stringValue
+            let sigs = index["veranstalter"].stringValue
+            let obj = ["title": title, "body": body, "sigs": sigs]
+            objects.append(obj)
+        }
+        
+        tableView.reloadData()
+    }
+    
+
+    override func viewWillAppear(animated: Bool) {
+        self.clearsSelectionOnViewWillAppear = self.splitViewController!.collapsed
+        super.viewWillAppear(animated)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow {
+                let object = objects[indexPath.row]
+                let controller = (segue.destinationViewController as! UINavigationController).topViewController as! DetailViewController
+                controller.detailItem = object
+                controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
+                controller.navigationItem.leftItemsSupplementBackButton = true
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+
+        let object = objects[indexPath.row]
+        cell.textLabel!.text = object["title"]
+        cell.detailTextLabel!.text = object["body"]
+        return cell
+    }
+
+   
+    
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Events in Berlin"
+    }
+
+    override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 80
+        
+    }
+   
+}
+
